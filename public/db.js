47545a9f4a454e57a15492e43f391234c1ac6ced
jesperingels <jesper.ingels@gmail.com>
2019-04-19 10:47:58 +0200

    create db.js

--- public/db.js
+++ public/db.js
@@ -1,15 +1,3 @@
-# Real-Time Web @cmda-minor-web · 2018-2019
-
-## Concept
-**De slimste stad van Nederland:** Meerdere mensen verbinden met de webapp. Hun locatie wordt in een overzicht getoond.
-Bijvoorbeeld: Amsterdam, Utrecht, Rotterdam. De mensen uit deze drie steden gaan enkele quizvragen beantwoorden en kunnen 
-zien wie de slimste stad van Nederland is. De muispositie van elke client wordt real-time op de server opgeslagen
-ook de locatie van de client wordt op de server real-time opgeslagen. Degene die als eerste op het juiste antwoord klikt 
-heeft gewonnen en scoort een punt voor zijn/haar stad. De vragen worden opgehaald uit een API die ik zelf aanmaak in Express.js
-
-## API
-Zo wordt is de data opgeslagen in mijn `db.js` file:
-```javascript
 quiz = [
     {
         question: 'Op de albumhoes van welk album van de Red Hot Chili Peppers zit een meisje achter de piano?',
@@ -90,29 +78,6 @@ quiz = [
         incorrect2: '1960',
         incorrect3: '1964'
     }
+
+
 ];
-```
-
-
-
-<!-- Add a link to your live demo in Github Pages 🌐-->
-
-<!-- ☝️ replace this description with a description of your own work -->
-
-<!-- Add a nice image here at the end of the week, showing off your shiny frontend 📸 -->
-
-<!-- Maybe a table of contents here? 📚 -->
-
-<!-- How about a section that describes how to install this project? 🤓 -->
-
-<!-- ...but how does one use this project? What are its features 🤔 -->
-
-<!-- What external data source is featured in your project and what are its properties 🌠 -->
-
-<!-- This would be a good place for your data life cycle ♻️-->
-
-<!-- Maybe a checklist of done stuff and stuff still on your wishlist? ✅ -->
-
-<!-- How about a license here? 📜 (or is it a licence?) 🤷 -->
-
-[rubric]: https://docs.google.com/spreadsheets/d/e/2PACX-1vSd1I4ma8R5mtVMyrbp6PA2qEInWiOialK9Fr2orD3afUBqOyvTg_JaQZ6-P4YGURI-eA7PoHT8TRge/pubhtml
