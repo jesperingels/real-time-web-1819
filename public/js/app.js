10eb17beb76c67abdc8f412606d6141f06bee5ca
jesperingels <jesper.ingels@gmail.com>
2019-04-23 14:44:03 +0200

    deploy api and app

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,48 @@
 (() => {
+    const socket = io();
+    const log = document.querySelector('.log');
+
+    let onlineUsers = [];
+
+    socket.on('user connect', (name) => {
+
+        while (log.firstChild) {
+            log.removeChild(log.firstChild);
+        }
+
+        name.forEach((item) => {
+
+            const list = document.createElement('li');
+            list.innerText = item + ' is connected';
+            log.appendChild(list);
+
+        });
+
+        console.log(name);
+        console.log(onlineUsers);
+
+    });
+
+    socket.on('user disconnect',(user) => {
+
+        while (log.firstChild) {
+            log.removeChild(log.firstChild);
+        }
+
+        user.forEach((item)=>{
+            const list = document.createElement('li');
+
+            list.innerText = item + ' is connected';
+            log.appendChild(list);
+        })
+
+    });
+
+    socket.on('start quiz', () => {
+        window.location.href = '/quiz';
+        console.log('start quiz');
+    })
+
 
 })();
 
--- src/index.js
+++ src/index.js
@@ -22,6 +22,71 @@ app.get('/api/v1/quiz', (req, res) => {
     })
 });
 
+let onlineUsers = [];
+
+app.get('/', (req, res) => {
+    res.render('pages/register.ejs');
+});
+
+app.post('/', (req, res) => {
+    console.log('form sent');
+
+    // on user connected
+    io.on('connection', socket => {
+        let thisUser = req.body.user;
+        console.log('a user connected');
+
+        if(onlineUsers.includes(thisUser)) {
+            console.log('user exists')
+        } else {
+            onlineUsers.push(thisUser);
+        }
+        console.log(onlineUsers.length + onlineUsers);
+        io.emit("user connect", onlineUsers);
+
+        // on user disconnect
+        socket.on("disconnect",() => {
+            console.log(thisUser + " disconnected");
+
+            for( let i = 0; i < onlineUsers.length; i++){
+                if ( onlineUsers[i] === thisUser) {
+                    onlineUsers.splice(i, 1);
+                }
+            }
+
+            io.emit("user disconnect", onlineUsers);
+        });
+
+        if(onlineUsers.length === 2) {
+
+            rp(options())
+                .then(res => {
+                    console.log(res);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+
+            io.emit('start quiz');
+        }
+
+    });
+
+    res.render('pages/lobby.ejs');
+});
+
+app.get('/quiz', (req, res) => {
+    res.render('pages/quiz.ejs');
+});
+
+
+
+const options = () => {
+    return {
+        url:`localhost:3008/api/v1/quiz`,
+    }};
+
+
 http.listen(port, () => console.log('App listening on port: ' + port ));
 
 
