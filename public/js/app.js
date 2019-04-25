5d2e584f837299b825322d859c0ba87a846d147e
jesperingels <jesper.ingels@gmail.com>
2019-04-25 13:26:08 +0200

    state = register and lobby page

--- public/js/app.js
+++ public/js/app.js
@@ -5,93 +5,162 @@
     const input = document.getElementById('username');
 
     let onlineUsers = [];
-    console.log(onlineUsers);
+
 
     if (form) {
+        // On form submit
         form.addEventListener('submit', function(e) {
-
+            console.log(input.value);
             const query = input.value;
+            // Prevent default form functionality
             e.preventDefault();
+
+            // Send event 'query' to server with input value
             socket.emit('query', query);
+
+            // clear input field
             input.value = "";
-            window.location.href = '/lobby';
+            form.submit();
+            console.log(query)
         });
     }
 
 
-
+    // On received event 'user connect' from server
     socket.on('user connect', (name) => {
-        console.log('je ziet mij!');
+        console.log('user connected!');
         console.log(name);
+
+        // clear all HTML in the log
         if (log) {
             while (log.firstChild) {
                 log.removeChild(log.firstChild);
             }
-
             console.log('naam'+name);
 
-            name.forEach((item) => {
-
-                const list = document.createElement('li');
-                list.innerText = item + ' is connected';
-                log.appendChild(list);
-
-            });
-
-
+            // Check if there are no users connected
+            if(name.length === 0) {
+                console.log('0 users connected');
+            }
+            // If users are still connected
+            else {
+                // For each user, add the user to log
+                name.forEach((item) => {
+                    // Add "'user' is connected" to log
+                    const list = document.createElement('li');
+                    list.innerText = item + ' is connected';
+                    log.appendChild(list);
+                });
+            }
         }
-
     });
 
-    socket.on('user disconnect',(user) => {
+    // On received event 'user disconnect' from server
+    socket.on('user disconnect', (user) => {
         console.log(user);
+        console.log('user disconnected');
 
+        // clear all HTML in the log
         if (log) {
             while (log.firstChild) {
                 log.removeChild(log.firstChild);
             }
 
-            user.forEach((item)=>{
-                const list = document.createElement('li');
-
-                list.innerText = item + ' is connected';
-                log.appendChild(list);
-            })
+            // Check if there are no users connected
+            if(name.length === 0) {
+                console.log('0 users connected');
+            }
+            // If users are still connected
+            else {
+                // For each user, add the user to log
+                name.forEach((item) => {
+                    // Add "'user' is connected" to log
+                    const list = document.createElement('li');
+                    list.innerText = item + ' is connected';
+                    log.appendChild(list);
+                });
+            }
         }
 
 
     });
 
     socket.on('start quiz', (data) => {
-        // Change URL to quiz
-        if(window.location.href.indexOf('/quiz') > 0) {
-            console.log('quiz has started!')
-        } else {
-            window.location.replace('/quiz')
-        }
+        // // Change URL to quiz
+        // if(window.location.href.indexOf('/quiz') > 0) {
+        //     console.log('quiz has started!')
+        // } else {
+        //     window.location.href = '/quiz';
+        // }
+
+        const question = document.getElementById('question');
+        const subTitle = document.getElementById('subtitle');
+        const answers = document.querySelector('.answers');
+        const log = document.querySelector('.log');
+        answers.style.display = 'block';
+        log.style.display = 'none';
+
+        question.innerText = '';
+        subTitle.innerText = '';
+        // answers.innerHTML = '';
+
         initQuiz(data);
         console.log('start quiz');
     });
 
     function initQuiz (data) {
+        console.log('initquiz is started');
+        console.log(data);
         const question = document.getElementById('question');
         const answerList = document.querySelector('.answers');
-        const list1 = document.createElement('li');
-        const list2 = document.createElement('li');
-        const list3 = document.createElement('li');
-        const list4 = document.createElement('li');
+
+        // const list1 = document.getElementById('list1');
+        // const list2 = document.getElementById('list2');
+        // const list3 = document.getElementById('list3');
+        // const list4 = document.getElementById('list4');
+
+        const list = document.querySelectorAll('listItem');
+
+        const listArr = [data[0].rightAnswer, data[0].incorrect3, data[0].incorrect2, data[0].incorrect1];
+
+        // const listArr = [
+        //     {
+        //         list: document.createElement('li'),
+        //         button1: document.createElement('input')
+        //     },
+        //     {
+        //         list: document.createElement('li'),
+        //         button2: document.createElement('input')
+        //     },
+        //     {
+        //         list3: document.createElement('li'),
+        //         button3: document.createElement('input')
+        //     },
+        //     {
+        //         list4: document.createElement('li'),
+        //         button4: document.createElement('input')
+        //     }];
+
         question.innerText = data[0].question;
-        list1.innerText = data[0].rightAnswer;
-        list2.innerText = data[0].incorrect3;
-        list3.innerText = data[0].incorrect2;
-        list4.innerText = data[0].incorrect1;
-        const listArr = [list1, list2, list3, list4];
+        // list1.innerText = data[0].rightAnswer;
+        // list2.innerText = data[0].incorrect3;
+        // list3.innerText = data[0].incorrect2;
+        // list4.innerText = data[0].incorrect1;
 
         const ranList = shuffle(listArr);
 
-        ranList.forEach((item)=>{
-            answerList.appendChild(item);
-        })
+        console.log(ranList);
+        console.log(list);
+
+        // list.forEach((item) => {
+        //     answerList.appendChild(item);
+        // })
+
+        for (let i = 0; i > list.length; i++) {
+            list[i].innerText = 'test';
+            console.log(list);
+            answerList.appendChild(list[i]);
+        }
 
     }
 
@@ -99,18 +168,13 @@
         var i = array.length,
             j = 0,
             temp;
-
         while (i--) {
-
             j = Math.floor(Math.random() * (i+1));
-
             // swap randomly chosen element with current element
             temp = array[i];
             array[i] = array[j];
             array[j] = temp;
-
         }
-
         return array;
     }
 
--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ app.get('/', (req, res) => {
     io.on('connection', socket => {
 
         socket.on('query', user => {
+            console.log('event query found');
             console.log(user);
             if(onlineUsers.includes(user)) {
                 console.log('user exists')
@@ -39,7 +40,15 @@ app.get('/', (req, res) => {
             console.log(onlineUsers);
         });
     });
+    // const options = () => {
+    //     return {
+    //         url:`https://slimste-stad-van-nederland.herokuapp.com/api/v1/quiz`,
+    //     }
+    // };
+
 });
+
+
 
 
 
@@ -48,49 +57,50 @@ app.get('/lobby', (req,res) =>{
 
     // on user connected
     io.on('connection', socket => {
+        console.log('a user connected in the lobby');
         io.emit("user connect", onlineUsers);
 
         // on user disconnect
-        socket.on("disconnect",(thisUser) => {
-            console.log(thisUser + " disconnected");
+        socket.on("disconnect",(reason) => {
+            console.log('user disconnected');
+            console.log("client disconnect because of " + reason);
 
-            for( let i = 0; i < onlineUsers.length; i++){
-                if ( onlineUsers[i] === thisUser) {
-                    onlineUsers.splice(i, 1);
-                }
-            }
+            // for (let i = 0; i < onlineUsers.length; i++) {
+            //     if ( onlineUsers[i] === thisUser) {
+            //         onlineUsers.splice(i, 1);
+            //     }
+            // }
 
             io.emit("user disconnect", onlineUsers);
         });
-        if(onlineUsers.length === 2) {
 
-            rp(options())
-                .then(res => {
-                    const data = JSON.parse(res);
-                    console.log(data);
-                    io.emit('start quiz', data.db);
-                })
-                .catch(function (err) {
-                    console.log(err);
-                });
-        }
     });
 
+    if(onlineUsers.length === 2) {
+        console.log('two users');
 
+        rp('https://slimste-stad-van-nederland.herokuapp.com/api/v1/quiz')
+            .then(res => {
+                const data = JSON.parse(res);
+                console.log(data);
+                console.log('start quiz');
+                io.emit('start quiz', data.db);
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+    } else {
+        console.log('not two users');
+    }
         res.render('pages/lobby.ejs');
     });
 
 
 app.get('/quiz', (req, res) => {
+
     res.render('pages/quiz.ejs');
 });
 
-
-
-const options = () => {
-    return {
-        url:`https://slimste-stad-van-nederland.herokuapp.com/api/v1/quiz`,
-    }};
 
 
 http.listen(port, () => console.log('App listening on port: ' + port ));
