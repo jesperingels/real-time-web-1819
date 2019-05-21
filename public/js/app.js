b03549f8e0bc9716df7dbf77f18b4ccb7df675b4
jesperingels <jesper.ingels@gmail.com>
2019-05-21 17:44:24 +0200

    Add user functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,60 @@
 (() => {
 
-    // const socket = io();
+    const socket = io();
+    // const user = document.querySelectorAll('user');
+    const userWindow = document.getElementById('online-users');
+    const main = document.querySelector('main');
+    let onlineUsers = [];
+
+    socket.on('user connect', name => {
+
+        // on user connect clear online users and userWindow
+        onlineUsers.length = 0;
+        userWindow.innerHTML = '';
+
+        if(main) {
+
+            console.log(name);
+            console.log(onlineUsers);
+
+            name.forEach( user => {
+
+                onlineUsers.push(user);
+
+
+
+                const ball = document.createElement('div');
+                ball.setAttribute('id', 'ball');
+                main.appendChild(ball);
+            });
+
+            onlineUsers.forEach(onlineUser => {
+                const userName = document.createElement('div');
+                userName.innerText = onlineUser;
+                userWindow.appendChild(userName);
+            });
+
+
+
+        }
+    });
+
+    socket.on('user disconnect', stillOnlineUsers => {
+        onlineUsers.length = 0;
+        userWindow.innerHTML = '';
+
+        stillOnlineUsers.forEach(user => {
+            onlineUsers.push(user);
+        });
+
+        onlineUsers.forEach(onlineUser => {
+            const userName = document.createElement('div');
+            userName.innerText = onlineUser;
+            userWindow.appendChild(userName);
+        });
+    });
+
+    // play audio on device orientation
     let xAxis = 0;
     let yAxis = 0;
     const jingle = document.getElementById('jingle');
@@ -8,7 +62,7 @@
     const dance = document.getElementById('dance');
     const spoken = document.getElementById('spoken');
 
-    let onlineUsers = [];
+
 
     // On device rotation
     window.ondeviceorientation = function (e) {
@@ -18,8 +72,8 @@
         let gamma = Math.floor(e.gamma);
 
         const ball = document.getElementById('ball');
-        const deviceData = document.querySelector('.device-data');
-        deviceData.innerHTML = 'Alpha:' + alpha + ' Beta:' + beta + ' Gamma:' + gamma;
+        // const deviceData = document.querySelector('.device-data');
+        // deviceData.innerHTML = 'Alpha:' + alpha + ' Beta:' + beta + ' Gamma:' + gamma;
 
 
         if(gamma < 0) {
@@ -80,67 +134,18 @@
 
 
     };
-    const button = document.getElementById('button');
-    button.addEventListener('click', () => {
-        jingle.play();
-        button.classList.add('hidden');
-    })
+
+    if (document.getElementById('button')) {
+        const button = document.getElementById('button');
+        button.addEventListener('click', () => {
+            jingle.play();
+            button.classList.add('hidden');
+        })
+    }
+
 
 
 })();
 
 
-
-// var bal = document.getElementById("bal")
-// var horizontaal = 0;
-// var verticaal = 0;
-//
-// window.ondeviceorientation = function rotatie(e){
-//     var a = Math.floor(e.alpha);
-//     var b = Math.floor(e.beta);
-//     var g = Math.floor(e.gamma);
-//     var tekst = "Alpha:"+ a + "<br>" + "Beta:" + b + "<br>" + "Gamma:" + g;
-//     bericht.innerHTML= tekst;
-//
-//     var alphaDiv = document.getElementById('alpha');
-//     var betaDiv = document.getElementById('beta');
-//     var gammaDiv = document.getElementById('gamma');
-//
-//
-//     if(g < 0){
-//         if(horizontaal < 300) {
-//             horizontaal = horizontaal + 5;
-//         }
-//     }
-//     else{
-//         if(horizontaal > 0) {
-//             horizontaal = horizontaal - 5;
-//
-//         }
-//     }
-//     bal.style.marginLeft = horizontaal + "px";
-//
-//     if(b < 0) {
-//         if(verticaal < 300) {
-//             verticaal = verticaal + 5;
-//         }
-//
-//     } else{
-//         if(verticaal > 0) {
-//             verticaal = verticaal - 5;
-//         }
-//
-//     }
-//
-//     bal.style.marginTop = verticaal + 'px';
-//     if(verticaal > 290) {
-//         bal.style.backgroundColor = 'pink';
-//     }
-//     else {
-//         if (verticaal < 10) {
-//             bal.style.backgroundColor = 'blue';
-//         }
-//     }
-// }
-//
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const rp = require('request-promise');
 const http = require('http').Server(app);
 const request = require('request');
-// const io = require('socket.io')(http);
+const io = require('socket.io')(http);
 const randomInt = require('random-int');
 const port = process.env.PORT || 3008;
 const bodyParser = require('body-parser');
@@ -20,43 +20,43 @@ app.use(bodyParser.urlencoded({extended: true}));
 let onlineUsers = [];
 
 app.get('/', (req, res) => {
-    res.render('pages/index.ejs');
+    res.render('pages/login.ejs');
 });
 
 
-// app.post('/', function(req, res) {
-//
-//     console.log('form sent');
-//
-//     io.on('connection', socket => {
-//         let thisUser = req.body.user;
-//         console.log('a user connected');
-//
-//         if(onlineUsers.includes(thisUser)) {
-//             console.log('user exists')
-//         } else {
-//             onlineUsers.push(thisUser);
-//         }
-//         console.log('online users= ' + onlineUsers);
-//         io.emit("user connect", onlineUsers);
-//
-//
-//         socket.on("disconnect",(reason) => {
-//
-//             console.log(thisUser + " disconnected " + 'because ' + reason);
-//
-//             for( let i = 0; i < onlineUsers.length; i++){
-//                 if ( onlineUsers[i] === thisUser) {
-//                     onlineUsers.splice(i, 1);
-//                 }
-//             }
-//
-//             io.emit("user disconnect", onlineUsers);
-//         });
-//     });
-//
-//     res.render('pages/index.ejs');
-// });
+app.post('/', function(req, res) {
+
+    console.log('form sent');
+
+    io.on('connection', socket => {
+        let thisUser = req.body.user;
+        console.log('a user connected');
+
+        if(onlineUsers.includes(thisUser)) {
+            console.log('user exists')
+        } else {
+            onlineUsers.push(thisUser);
+        }
+        console.log('online users= ' + onlineUsers);
+        io.emit("user connect", onlineUsers);
+
+
+        socket.on("disconnect",reason => {
+
+            console.log(thisUser + " disconnected " + 'because ' + reason);
+
+            for( let i = 0; i < onlineUsers.length; i++){
+                if ( onlineUsers[i] === thisUser) {
+                    onlineUsers.splice(i, 1);
+                }
+            }
+
+            io.emit("user disconnect", onlineUsers);
+        });
+    });
+
+    res.render('pages/index.ejs');
+});
 
 http.listen(port, () => console.log('App listening on port: ' + port ));
 
